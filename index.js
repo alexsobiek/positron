c1d995d12670be3cd1676ad4bcec6949dd818470
Alex Sobiek <alex@alexsobiek.com>
2021-06-08 17:46:29 -0500

    Working patch creation system

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const asar = require("asar");
 const path = require("path");
+const diff = require("diff");
+
 
 module.exports = class {
     /**
@@ -24,14 +26,59 @@ module.exports = class {
         if (!fs.existsSync(asarPackage)) throw new Error("asarPackage not found.");
         if (!fs.existsSync(sourceDir)) fs.mkdirSync(sourceDir);
         if (!fs.existsSync(patchDir)) fs.mkdirSync(patchDir);
-
     }
 
     /**
      * Unpackages the asar package
      */
     unpackage = () => {
-        asar.extractAll(this.package, path.join(this.sourceDir, this.packageName));
-        asar.extractAll(this.package, path.join(this.sourceDir, ".positron", this.packageName));
+        asar.extractAll(this.package, path.join(__dirname, this.sourceDir, this.packageName));
+        asar.extractAll(this.package, path.join(__dirname, this.sourceDir, ".positron", this.packageName));
+    }
+
+    /**
+     * Creates patches for any modified files
+     */
+    makePatches = (directory) => {
+        const dir = (directory) ? directory : this.sourceDir;
+        const files = fs.readdirSync(dir);
+        files.forEach((file) => {
+            const sourcePath = path.join(dir, file);
+            const name = sourcePath.split("/").pop();
+            const localDir = sourcePath.slice(this.sourceDir.length)
+            const patchPath = path.join(this.patchDir, localDir);
+
+            if (fs.statSync(sourcePath).isDirectory() && !sourcePath.endsWith("node_modules")) {
+                if (!fs.existsSync(patchPath)) fs.mkdirSync(patchPath, { recursive: true });
+                this.makePatches(sourcePath);
+            } else {
+                if (!file.endsWith("node_modules")) {
+                    console.log("Checking " + sourcePath);
+                    fs.readFile(sourcePath, 'utf8',(err, fileData) => {
+                        if (err) throw err;
+                        fs.readFile(path.join(this.sourceDir, "..", ".positron", localDir), 'utf8', (err, originalData) => {
+                            if (err) throw err;
+                            const patchData = diff.createPatch(name, fileData, originalData, "", "");
+                            const patchFile = patchPath + ".patch";
+                            if (patchData.split(/\r\n|\r|\n/).length > 5) {
+                                if (fs.existsSync(patchFile)) fs.unlinkSync(patchFile);
+                                fs.writeFile(patchFile, patchData, function (err) {
+                                    if (err) throw err;
+                                    console.log("Created patch: " + patchFile);
+                                });
+                            } else if (fs.existsSync(patchFile)) fs.unlinkSync(patchFile);
+                        });
+                    });
+                }
+            }
+        })
+    }
+
+    applyPatches = () => {
+
+    }
+
+    package = () => {
+
     }
 }
