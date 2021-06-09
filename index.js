e1886d7fca7be7388ae53468f59a7eadff093568
Alex Sobiek <alex@alexsobiek.com>
2021-06-08 22:57:43 -0500

    Working patch system

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ module.exports = class {
         files.forEach((file) => {
             const sourcePath = path.join(dir, file);
             const name = sourcePath.split("/").pop();
-            const localDir = sourcePath.slice(this.sourceDir.length)
+            const localDir = sourcePath.slice(this.sourceDir.length);
             const patchPath = path.join(this.patchDir, localDir);
 
             if (fs.statSync(sourcePath).isDirectory() && !sourcePath.endsWith("node_modules")) {
@@ -58,11 +58,11 @@ module.exports = class {
                         if (err) throw err;
                         fs.readFile(path.join(this.sourceDir, "..", ".positron", localDir), 'utf8', (err, originalData) => {
                             if (err) throw err;
-                            const patchData = diff.createPatch(name, fileData, originalData, "", "");
+                            const patchData = diff.createPatch(name, originalData, fileData, "", "");
                             const patchFile = patchPath + ".patch";
                             if (patchData.split(/\r\n|\r|\n/).length > 5) {
                                 if (fs.existsSync(patchFile)) fs.unlinkSync(patchFile);
-                                fs.writeFile(patchFile, patchData, function (err) {
+                                fs.writeFile(patchFile, patchData, (err) => {
                                     if (err) throw err;
                                     console.log("Created patch: " + patchFile);
                                 });
@@ -71,11 +71,38 @@ module.exports = class {
                     });
                 }
             }
-        })
+        });
     }
 
-    applyPatches = () => {
+    applyPatches = (directory) => {
+        const dir = (directory) ? directory : this.patchDir;
+        const files = fs.readdirSync(dir);
+        files.forEach((file) => {
+            const sourcePath = path.join(dir, file);
+            const localDir = sourcePath.slice(this.patchDir.length);
+            console.log(localDir);
+            if (fs.statSync(sourcePath).isDirectory()) this.applyPatches(sourcePath);
+            else {
+                const destPath = path.join(this.sourceDir, localDir.slice(0, -6));
+                console.log("Applying patch " + sourcePath + " to " + destPath);
 
+                fs.readFile(sourcePath, 'utf8', (err, sourceData) => {
+                   if (err) throw err;
+                   fs.readFile(destPath, 'utf8', (err, destData) => {
+                       if (err) throw err;
+
+                       const newData = diff.applyPatch(destData, sourceData);
+                       console.log(newData);
+                       if (newData !== false) {
+                           fs.writeFile(destPath, newData, (err) => {
+                               if (err) throw err;
+                               console.log("Applied patch to " + destPath);
+                           });
+                       } else console.log("Skipping patch for " + destPath);
+                   })
+                });
+            }
+        });
     }
 
     package = () => {
